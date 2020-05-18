55fc7a5b804972bbef86b4b7d9d31491f4dc3379
Laffery <2387065420@qq.com>
2020-05-18 10:01:16 +0800

    :seedling: init

---
+++ lab3.cpp
@@ -0,0 +1,23 @@
+#include "common.h"
+
+using namespace std;
+
+//You should only code here.Don't edit any other files in this 
+int func1(int amount, vector<int>& coins)
+{	
+
+	return -1;
+
+}
+
+int func2(int amount, vector< vector<int> >& conquer)
+{
+	
+	return -1;
+}
+
+
+double func3(int n,int hp,vector<int>& damage,vector<int>& edges) {
+    
+    return -1;
+}
